9d1201b8128079f4cc59de9c5c782ea724c04e5a
Bestun Gardi <bestun95@yahoo.com>
2018-09-01 23:58:59 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -21,5 +21,3 @@ ReactDOM.render(
   <MovieCard title="Mad Max: Fury Road" poster="http://image.tmdb.org/t/p/w342/kqjL17yufvn9OVLyXYpvtyrFfak.jpg" genres={['Action', 'Adventure', 'Science Fiction', 'Thriller']} />,
   document.getElementById('root')
 );
-  document.getElementById('root')
-);
