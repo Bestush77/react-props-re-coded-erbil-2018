62c9f5bed60dead5010537e77f10c7575ae08f46
Bestun Gardi <bestun95@yahoo.com>
2018-09-01 23:58:11 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -5,15 +5,17 @@ import ReactDOM from 'react-dom';
 class MovieCard extends React.Component {
   render() {
     return (
-      <div className="movie-card">
-        <img src="http://image.tmdb.org/t/p/w342/kqjL17yufvn9OVLyXYpvtyrFfak.jpg" alt="Mad Max: Fury Road" />
-        <h2>Mad Max: Fury Road</h2>
-        <small>Genres: Action, Adventure, Science Fiction, Thriller</small>
+     <div className="movie-card">
+        <img  
+          src={this.props.poster} 
+          alt={this.props.title} 
+        />
+        <h2>{this.props.title}</h2>
+        <small>Genres: {this.props.genres.join(', ')}</small>
       </div>
     );
   }
 }
- 
 ReactDOM.render(
   ReactDOM.render(
   <MovieCard title="Mad Max: Fury Road" poster="http://image.tmdb.org/t/p/w342/kqjL17yufvn9OVLyXYpvtyrFfak.jpg" genres={['Action', 'Adventure', 'Science Fiction', 'Thriller']} />,
