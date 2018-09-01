613ad9e2c6c8e2b217a9657c6f0e388834b79c38
Bestun Gardi <bestun95@yahoo.com>
2018-09-01 23:59:21 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -19,5 +19,4 @@ class MovieCard extends React.Component {
 ReactDOM.render(
   ReactDOM.render(
   <MovieCard title="Mad Max: Fury Road" poster="http://image.tmdb.org/t/p/w342/kqjL17yufvn9OVLyXYpvtyrFfak.jpg" genres={['Action', 'Adventure', 'Science Fiction', 'Thriller']} />,
-  document.getElementById('root')
-);
+  document.getElementById('root');
